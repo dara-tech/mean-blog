f4bdbeeb6de36f0e20838a0475c70dbf4628443c
Dara <97618988+dara-tech@users.noreply.github.com>
2024-02-23 21:07:27 +0700

    complete signin page functional

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -1,5 +1,125 @@
+import { Alert, Button, Label, Spinner, TextInput } from "flowbite-react";
+import { Link, useNavigate} from "react-router-dom";
+import { useState } from "react";
+
 export default function SignIn() {
+
+    const [formData, setFormData]= useState({});
+    const [errorMessage, setErrorMessage] = useState(null);
+    const [loading, setLoading] = useState(false);
+    const navigate =useNavigate();
+
+    const handleChange = (e) => {
+        setFormData ({...formData, [e.target.id]: e.target.value.trim() });
+    };
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        if (!formData.email || !formData.password) {
+            return setErrorMessage('Please fill out all fields!');
+        }
+        try {
+            setLoading(true);
+            setErrorMessage(null);
+            const res = await fetch('/api/auth/signin', {
+                method: 'POST',
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify(formData),
+            });
+            const data = await res.json(); // Parse the JSON response once
+            setLoading(false); // Set loading state after parsing JSON
+            
+            if (res.ok) {
+                console.log('Sign up successful!', data);
+                // Do something with the successful response data
+                // For example, you can redirect the user to another page
+                // window.location.href = '/success-page';
+            } else {
+                console.error('Sign up failed:', res.statusText);
+                // Handle failed response
+                setErrorMessage(data.message); // Set error message from response data
+            }
+            setLoading(false);
+            if(res.ok){
+                navigate('/')
+            }
+            
+        } catch (error) {
+            console.error('An error occurred:', error.message);
+            setErrorMessage('An error occurred. Please try again.'); // Set generic error message
+            setLoading(false);
+        }
+    };
+    
+    
+    
+    
+
     return (
-    <div>SignIn</div>
+        <div className="min-h-screen mt-20">
+            <div className="flex flex-col md:flex-row md:items-center p-3 max-w-3xl mx-auto">
+                {/* Left Section */}
+                <div className="md:w-1/2 md:pr-10">
+                    <Link to='/' className="font-bold dark:text-white text-4xl block">
+                        <span className="px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to bg-pink-500 rounded-b-lg text-white">Sovandara</span> Blog
+                    </Link>
+                    <p className="text-sm mt-5">
+                        This blog is a treasure trove of new discoveries and insights, offering a captivating journey through his latest research findings. From the Homepage featured articles to the dedicated sections on various topics, visitors are invited to explore a world of knowledge at their fingertips.
+                        you can sign in with your email and password or with google.
+                    </p>
+                </div>
+                {/* Right Section */}
+                <div className="md:w-1/2 mt-8 md:mt-0">
+                    <form className="flex flex-col gap-4" onSubmit={handleSubmit}>
+                
+                        {/* Email */}
+                        <div>
+                            <Label value='Your Email'/>
+                            <TextInput 
+                                type="email"
+                                placeholder="Email"
+                                id='email' onChange ={handleChange}
+                            /> 
+                        </div>
+                        {/* Password */}
+                        <div>
+                            <Label value='Your Password'/>
+                            <TextInput 
+                                type="password" 
+                                placeholder="Password"
+                                id='password' onChange ={handleChange}
+                            />
+                        </div>
+                        {/* Sign Up Button */}
+                        <Button gradientDuoTone='purpleToPink' type='submit' disabled ={loading}>
+                            {
+                                loading? (
+                                    <>
+                                    <Spinner size='sm'/>
+                                    <span className="pl-3">loading...</span>
+                                    </>
+                                    
+                                ): 'Sign in'
+                            }
+                        </Button>
+                    </form>
+                    {/* Sign In Link */}
+                    <div className="flex gap-2 text-sm mt-5">
+                        <span>Dont Have an account?</span>
+                        <Link to='/sign-up' className="text-blue-500">
+                            Sign Up
+                        </Link>
+                    </div>
+                    {
+    errorMessage && (
+        <Alert className='mt-5' color='failure'>
+            {errorMessage}
+        </Alert>
     )
+}
+
+                </div>
+            </div>
+        </div>
+    );
 }
