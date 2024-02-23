8ce4d2e237ca26dd127db4eaa17d70f9f65d6fd7
Dara <97618988+dara-tech@users.noreply.github.com>
2024-02-23 20:54:20 +0700

    create signin api route

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,6 +1,8 @@
 import User from '../models/user.model.js';
 import bcryptjs from 'bcryptjs';
 import { errorHanddler } from '../utils/error.js';
+import Jwt from 'jsonwebtoken';
+
 export const signup = async (req, res, next) => {
     const { username, email, password } = req.body;
     if (!username || !email || !password || username === '' || email ==='' || password === ''){
@@ -19,7 +21,42 @@ export const signup = async (req, res, next) => {
     } catch (error){
      next(error);
     }
-    // Save the new user to the database
-   
-    
 };
+export const signin = async (req, res, next) => {
+    const { email, password } = req.body;
+  
+    if (!email || !password || email === '' || password === '') {
+      next(errorHanddler(400, 'All fields are required'));
+    }
+  
+    try {
+      const validUser = await User.findOne({ email });
+      if (!validUser) {
+        return next(errorHanddler(404, 'User not found'));
+      }
+      const validPassword = bcryptjs.compareSync(password, validUser.password);
+      if (!validPassword) {
+        return next(errorHanddler(400, 'Invalid password'));
+      }
+
+
+
+      const token = Jwt.sign(
+        { id: validUser._id, isAdmin: validUser.isAdmin },
+        process.env.JWT_SECRET
+      );
+
+      const {password : pass, ...rest}= validUser._doc;
+
+            res
+            .status(200)
+            .cookie('acess_token',token, {
+            httpOnly: true,})
+            .json(rest);
+
+
+    } catch (error) {
+        next(error)
+    }
+};
+
