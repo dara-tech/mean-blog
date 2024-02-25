def606f4f4bb453ccbf588bfaffac30239a7d7b5
Dara <97618988+dara-tech@users.noreply.github.com>
2024-02-26 04:20:53 +0700

    add update post functionality

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -2,7 +2,10 @@ import { useEffect, useState } from 'react';
 import { useLocation } from 'react-router-dom';
 import DashSidebar from '../components/DashSidebar';
 import DashProfile from '../components/DashProfile';
-
+import DashPosts from '../components/DashPosts';
+// import DashUsers from '../components/DashUsers';
+// import DashComments from '../components/DashComments';
+// import DashboardComp from '../components/DashboardComp';
 
 export default function Dashboard() {
   const location = useLocation();
@@ -23,6 +26,8 @@ export default function Dashboard() {
       {/* profile... */}
       {tab === 'profile' && <DashProfile />}
       {/* posts... */}
+      {tab === 'posts' && <DashPosts />}
+  
     </div>
   );
 }
