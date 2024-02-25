f6d32aef81e1fc118aed703d4732d357c6572e1e
Dara <97618988+dara-tech@users.noreply.github.com>
2024-02-26 02:42:22 +0700

    Complete create a post page ui

--- client/src/components/DashProfile.jsx
+++ client/src/components/DashProfile.jsx
@@ -5,13 +5,14 @@ import { getDownloadURL, getStorage, ref, uploadBytesResumable } from 'firebase/
 import { app } from '../firebase';
 import { CircularProgressbar } from 'react-circular-progressbar';
 import 'react-circular-progressbar/dist/styles.css';
-import { updateFailure, updateStart,updateSuccess,deleteUserStart,deleteUserSuccess,deleteUserFailure,signoutSuccess } from '../redux/user/userSlice';
+import { updateFailure, updateStart,updateSuccess,deleteUserStart,deleteUserSuccess,deleteUserFailure,signoutSuccess, } from '../redux/user/userSlice';
 import { HiOutlineExclamationCircle } from 'react-icons/hi';
+import {Link } from 'react-router-dom'
 
 
 
 export default function DashProfile() {
-  const { currentUser} = useSelector(state => state.user);
+  const { currentUser ,error,loading} = useSelector((state )=> state.user);
   const [imageFile, setImageFile] = useState(null);
   const [imageFileUrl, setImageFileUrl] = useState(null);
   const [imageFileUploadProgress, setImageFileUploadProgress] = useState(null);
@@ -197,9 +198,23 @@ export default function DashProfile() {
         <TextInput type='email' id='email' placeholder='Email' defaultValue={currentUser.email} onChange={handleChange} />
         <TextInput type='password' id='password' placeholder='Password' onChange={handleChange} />
 
-        <Button type='submit' gradientDuoTone='purpleToBlue' outline>
-          Update
+        <Button type='submit' gradientDuoTone='purpleToBlue' outline disabled={loading ||  imageFileUploading}>{loading ? 'loading ': 'Update'}
+
         </Button>
+        {currentUser.isAdmin && (
+          <Link to={'/create-post'}>
+            <Button
+              type='button'
+              gradientDuoTone='purpleToPink'
+              className='w-full'
+            >
+              Create a post
+            </Button>
+          </Link>
+        )}
+
+
+
       </form>
       <div className='text-red-500 flex justify-between mt-5'>
       <span onClick={() => setShowModal(true)} className='cursor-pointer'>
@@ -219,6 +234,9 @@ export default function DashProfile() {
           {updateUserError}
         </Alert>
       )}
+
+
+
       <Modal
         show={showModal}
         onClose={() => setShowModal(false)}
