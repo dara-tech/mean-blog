1f77e12e81232f9fe038d64a09d1548417d8d4bd
Dara <97618988+dara-tech@users.noreply.github.com>
2024-02-29 20:34:43 +0700

    update header

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -48,21 +48,20 @@ export default function Header() {
       };
       
     return (
-        
-        <Navbar className="border shadow">
-            
-            
-            
-            <div className="flex gap-2 md:order-2 ">
-              <Link to='/' className="self-center whitespace-nowrap text-sm:text-xl font-semibold dark:text-white">
+        <Navbar className="border shadow ">
+            <Link to='/' className="text-sm:text-xl font-semibold dark:text-white">
                 <span className="px-2 py-2 bg-gradient-to-r from-indigo-500 via-purple-500 to bg-pink-500 rounded-lg text-white">Dara</span>
                 Blog
-            </Link><form onSubmit={handleSubmit}>
+            </Link>
+            
+            
+            <div className="flex gap-2 md:order-2">
+              <form onSubmit={handleSubmit}>
                <TextInput
                 type='text'
                 placeholder='Search...'
                 rightIcon={AiOutlineSearch}
-               
+                className='w-24'
                 value={searchTerm}
                 onChange={(e) => setSearchTerm(e.target.value)}
                 />
