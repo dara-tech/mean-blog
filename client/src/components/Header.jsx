ff20af4ef36705be08ae9fd7347eaae385e03ff5
Dara <97618988+dara-tech@users.noreply.github.com>
2024-02-29 17:11:30 +0700

    edit nav bar

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -19,6 +19,7 @@ export default function Header() {
     useEffect(() => {
         const urlParams = new URLSearchParams(location.search);
         const searchTermFromUrl = urlParams.get('searchTerm');
+        
         if (searchTermFromUrl) {
           setSearchTerm(searchTermFromUrl);
         }
@@ -47,26 +48,29 @@ export default function Header() {
       };
       
     return (
-        <Navbar className="border-b-2">
+        <Navbar className="border shadow-">
             <Link to='/' className="self-center whitespace-nowrap text-sm:text-xl font-semibold dark:text-white">
-                <span className="px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to bg-pink-500 rounded-lg text-white">Dara</span>
+                <span className="px-2 py-2 bg-gradient-to-r from-indigo-500 via-purple-500 to bg-pink-500 rounded-lg text-white">Dara</span>
                 Blog
             </Link>
-            <form onSubmit={handleSubmit}>
-        <TextInput
-          type='text'
-          placeholder='Search...'
-          rightIcon={AiOutlineSearch}
-          className='hidden lg:inline'
-          value={searchTerm}
-          onChange={(e) => setSearchTerm(e.target.value)}
-        />
+            
+            
+            <div className="flex gap-2 md:order-2"><form onSubmit={handleSubmit}>
+            <TextInput
+  type='text'
+  placeholder='Search...'
+//   rightIcon={AiOutlineSearch}
+  className='lg:inline-block lg:w-auto ml-auto mr-2 border-lg'
+  value={searchTerm}
+  onChange={(e) => setSearchTerm(e.target.value)}
+/>
+
+
       </form>
-            <Button className="w-12 h-10 lg:hidden" color="gray" pill>
+                <Button className="w-10 h-10" color="gray" pill>
                 <AiOutlineSearch />
             </Button>
-            <div className="flex gap-2 md:order-2">
-                <Button className="w-12 h-10 hidden sm:inline" color='gray' pill 
+                <Button className="w-12 h-10  sm:inline" color='gray' pill 
                 onClick={()=>dispatch(toggleTheme())}>
                    {theme === 'light'? <FaSun/> : <FaMoon/>}
                 </Button>
